b77e20983158a67570fd2496daf471c612dd5f24
Brian Cullen <Bpcullen@yahoo.com>
2019-05-14 14:42:26 -0500

    xsxs

--- PingProject.py
+++ PingProject.py
@@ -1,12 +1,15 @@
 import os
 
 hostname = "192.168.5.139" #change to any ip on your network, im using my iphones ip.
+
 # you might have to change the -c to -n for windows
 response = os.system("ping -c 10 " + hostname)
 
 #and then check the response...
+
 if response == 0:
     print(hostname, 'is up!')
 else:
     print(hostname, 'is down!')
+
 
