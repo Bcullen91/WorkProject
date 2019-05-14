0122a3ebf426a390e8d4da40e3bb6992c1838a3b
David Leake <dleake14@gmail.com>
2019-05-14 13:37:59 -0500

    Android's are better

--- PingProject.py
+++ PingProject.py
@@ -1,11 +1,21 @@
 import os
 
-hostname = "192.168.5.139" #change to any ip on your network, im using my iphones ip.
+# DL Adding my own IP - because this is my code
+dlHostName = "192.168.1.213"
+# hostname = "192.168.5.139"
+#change to any ip on your network, im using my iphones ip.
 # you might have to change the -c to -n for windows
-response = os.system("ping -c 1 " + hostname)
+# response = os.system("ping -n 10 " + hostname)
+dlResponse = os.system("ping -n 10 " + dlHostName)
 
 #and then check the response...
-if response == 0:
-    print(hostname, 'is up!')
+# if response == 0:
+#     print(hostname, 'is up!')
+# else:
+#     print(hostname, 'is down!')
+
+
+if dlResponse == 0:
+    print(dlHostName, 'is up!')
 else:
-    print(hostname, 'is down!')
+    print(dlHostName, 'is down!')
