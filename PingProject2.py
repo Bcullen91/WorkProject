2ea5066c8284f13afaed10a38f3f39c8154f8bda
Brian Cullen <Bpcullen@yahoo.com>
2019-05-14 15:09:39 -0500

    Try this out

--- PingProject2.py
+++ PingProject2.py
@@ -1,12 +1,13 @@
 import platform    # For getting the operating system name
 import subprocess  # For executing a shell command
+import time
 
 def ping(host):
     """
     Returns True if host (str) responds to a ping request.
     Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
     """
-
+        #f=open("log.txt","a+")
     # Option for the number of packets as a function of
     param = '-n' if platform.system().lower()=='windows' else '-c'
 
@@ -14,7 +15,11 @@ def ping(host):
     command = ['ping', param, '1', host]
 
     return subprocess.call(command) == 0
-    print(param)
-    print(command)
-    
-ping("192.168.5.139")
+
+while ping("192.168.5.139") == True:
+    print("iPhone is Online")
+    time.sleep(1)
+else:
+    print("iPhone is now Offline")
+
+
