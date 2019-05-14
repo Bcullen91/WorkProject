291c30d47b1171a1ff24dbee80ed742b027e7ab8
Brian Cullen <Bpcullen@yahoo.com>
2019-05-14 15:30:13 -0500

    constantly running

--- PingProject2.py
+++ PingProject2.py
@@ -7,19 +7,21 @@ def ping(host):
     Returns True if host (str) responds to a ping request.
     Remember that a host may not respond to a ping (ICMP) request even if the host name is valid.
     """
-        #f=open("log.txt","a+")
     # Option for the number of packets as a function of
     param = '-n' if platform.system().lower()=='windows' else '-c'
-
     # Building the command. Ex: "ping -c 1 google.com"
     command = ['ping', param, '1', host]
-
     return subprocess.call(command) == 0
 
-while ping("192.168.5.139") == True:
-    print("iPhone is Online")
-    time.sleep(1)
-else:
-    print("iPhone is now Offline")
+run= str("yes")
+while run == "yes":
+    print("Looping")
+    while ping("192.168.5.139") == True:
+        print("iPhone is Online")
+        time.sleep(1)
+    else:
+        print("iPhone is now Offline")
+        continue
+time.sleep(15)
 
 
