bd3d6bab4a51da31c59ae7ce82004dc6270a62ff
Brian Cullen <Bpcullen@yahoo.com>
2019-05-15 10:13:26 -0500

    Added comments

--- PingProject2.py
+++ PingProject2.py
@@ -13,19 +13,19 @@ def ping(host):
     command = ['ping', param, '1', host]
     return subprocess.call(command) == 0
 
-waittime= int(0)
-running= str("yes")
-while running == "yes":
-    while waittime > 0:
+waittime= int(0)  # Sets waittime to 0 so it will immediately start running
+running= str("yes")  # Sets the variable running to yes so it will always run constantly
+while running == "yes":  # Starts the infinite loops since running will never not be "yes"
+    while waittime > 0:  # If waittime is greater than 0 it prints the waitime and then waits 1 second
         print(waittime)
         time.sleep(1)
         waittime = waittime - 1
         continue
-    while waittime == 0:
-        while ping("192.168.5.139") == True:
+    while waittime == 0:  # If waittime is 0 it will run the ping command. 
+        while ping("192.168.5.139") == True:  # if the ping is successful it will return True
             print("iPhone is Online")
             time.sleep(1)
-        else:
+        else:  #If the ping fails it will add to the waittime and then loop to ping again
             print("iPhone is now Offline")
             waittime= waittime + 3
             print("Looping")
