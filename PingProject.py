9cd383f4424124bb0cedd8fa93fd431ee9855387
Brian Cullen <Bpcullen@yahoo.com>
2019-05-21 14:46:48 -0500

    working relay

--- PingProject.py
+++ PingProject.py
@@ -1,21 +1,22 @@
 import time
 import RPi.GPIO as GPIO
 
-# Pin definitions
-led_pin = 2
 
 # Suppress warnings
 GPIO.setwarnings(False)
 
 # Use "GPIO" pin numbering
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
 # Set LED pin as output
-GPIO.setup(led_pin, GPIO.OUT)
+GPIO.setup(12, GPIO.OUT)
 
-# Blink forever
-while True:
-    GPIO.output(led_pin, GPIO.HIGH) # Turn LED on
-    time.sleep(1)                   # Delay for 1 second
-    GPIO.output(led_pin, GPIO.LOW)  # Turn LED off
-    time.sleep(1)                   # Delay for 1 second
+
+GPIO.output(12, GPIO.HIGH) # Turn LED on
+print("x")
+time.sleep(1)                   # Delay for 1 second
+GPIO.output(12, GPIO.LOW)  # Turn LED off
+print("y")
+time.sleep(1)                   # Delay for 1 second
+GPIO.cleanup()
+print("Finshed")
