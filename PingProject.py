1ec394fa62aef379ab6fc8dfdd97af75d485a453
Brian Cullen <Bpcullen@yahoo.com>
2019-05-22 15:47:23 -0500

    wrnue

--- PingProject.py
+++ PingProject.py
@@ -1,5 +1,11 @@
-def test():
-    print("Testing")
+import platform    # For getting the operating system name
+import subprocess  # For executing a shell command
+import time
+print("This is working")
+reboottime = 10
 
-for i in range(0,10):
-    test()
+for i in range(reboottime, 0, -1):
+    print("Your device will be powered on in " + str(i) + " seconds")
+    time.sleep(1)
+
+print("Finished")
--- PingProject2.py
+++ PingProject2.py
@@ -3,7 +3,15 @@ import subprocess  # For executing a shell command
 import time
 import RPi.GPIO as GPIO
 
-host= str("192.168.5.137")   # Change this IP address to whatever you want to test connection to. 
+host= str("192.168.5.137")   # Change this IP address to whatever you want to test connection to.
+reboottime = 10
+timeafterreboot = 15
+sincereboot = 10     # Sets variable for since reboot so it wont keep power cycling (420 seconds for 7 minutes)
+offTime= int(0)         # Creates a variable that will be how long its been offline
+waittime= int(0)         # Sets waittime to 0 so it will immediately start running
+running= str("yes") 
+
+
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
@@ -11,13 +19,12 @@ GPIO.setup(12, GPIO.OUT)
 
 def reboot():
     GPIO.output(12, GPIO.HIGH)
-    for i in range(30,0):
+    for i in range(reboottime, 0, -1):
         print("Your device will be powered on in " + i + " seconds")
         time.sleep(1)
     GPIO.output(12, GPIO.LOW)
-    sincereboot = 420
+    sincereboot = timeafterreboot
     print("Your device has been rebooted and should be powering on now.")
-
 
 def ping(host):
     """
@@ -29,11 +36,6 @@ def ping(host):
     # Building the command. Ex: "ping -c 1 google.com" [ping, system, number of pings, host ip]
     command = ['ping', param, '1', host]
     return subprocess.call(command) == 0
-
-offTime= int(0)         # Creates a variable that will be how long its been offline
-waittime= int(0)         # Sets waittime to 0 so it will immediately start running
-running= str("yes") 
-sincereboot = int(420)     # Sets variable for since reboot so it wont keep power cycling (420 seconds for 7 minutes)
 
 while running == "yes":       # Starts the infinite loops since running will never not be "yes"
     while sincereboot > 0:
@@ -64,6 +66,5 @@ while running == "yes":       # Starts the infinite loops since running will nev
             print("The iPhone has been offline for " + str(offTime) + " seconds")
             break
     continue
-else
-    running= str("yes")
+else:
     print("Some error occurred and the the 'yes' loop failed.")
