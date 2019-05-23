266156ed0132c1700678d84d03db9b53fca1628b
David Leake <dleake14@gmail.com>
2019-05-23 15:17:36 -0500

    Reup1

--- PingProject.py
+++ PingProject.py
@@ -1,25 +1,26 @@
 import platform    # For getting the operating system name
 import subprocess  # For executing a shell command
 import time
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
 host= str("8.8.8.8")  
 
-sincereboot = int(20)     
+sincereboot = int(2)     
 offTime= int(0)         
 waittime= int(0)         
 running= str("yes") 
 
-GPIO.setwarnings(True)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(12, GPIO.OUT)
+# GPIO.setwarnings(True)
+# GPIO.setmode(GPIO.BOARD)
+# GPIO.setup(12, GPIO.OUT)
 
 def reboot():
-    GPIO.output(12, GPIO.HIGH)
-    for i in range(20, 0, -1):
-        print("Your device will be powered on in " + str(i) + " seconds")
+    # GPIO.output(12, GPIO.HIGH)
+    for i in range(3, 0, -1):
+        print("Your device will be powered on in: ",end="",flush=True)
+        print(str(i) + " seconds",end='\r',flush=True)
         time.sleep(1)
-    GPIO.output(12, GPIO.LOW)
+    # GPIO.output(12, GPIO.LOW)
     print("Your device has been rebooted and should be powering on now.")
 
 def ping(host):
@@ -29,34 +30,37 @@ def ping(host):
 
 while running == "yes":     
     while sincereboot > 0:
-        print("It has only been " + str(sincereboot) + " seconds since the last reboot")
+        print(" Time Since Last Reboot: ",end="")
+        print(str(sincereboot),end="\r", flush=True)
         time.sleep(1)
         sincereboot = sincereboot - 1
         continue
     while offTime > 20:
-        print("Your device is rebooting")
+        print(" Your device is rebooting")
         reboot()
         time.sleep(30)
         sincereboot = 20
-        print("Testing after the reboot")
+        print(" Testing after the reboot")
         offTime= 0
         break
     while waittime > 0:          
-        print("Waiting " + str(waittime) + " seconds")
+        print(" Waiting ",end="")
+        print(str(waittime),end="\r",flush=True)
         time.sleep(1)
         waittime = waittime - 1
         continue
     while waittime == 0:           
         while ping(host) == True:
-            print("Google is Online")
+            print(" Google is Online")
             waittime= 3
             offTime= 0
             break
         else:           
-            print("Google is now Offline")
+            print(" Google is now Offline")
             offTime= offTime + 7
-            print("The Google has been offline for " + str(offTime) + " seconds")
+            print(" The Google has been offline for: ",end="")
+            print(str(offTime) + " seconds",end='\r', flush=True)
             break
     continue
 else:
-    print("Some error occurred and the the 'yes' loop failed.")
+    print(" Some error occurred and the the 'yes' loop failed.")
